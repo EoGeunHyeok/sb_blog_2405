d9f5e403b60558a0793ed44dc5eee2fd364a7c66
EoGeunHyeok <eghsamo@naver.com>
2024-05-23 17:51:08 +0900

    로그인 기능 구현을 위한 세팅

--- src/main/java/com/example/blog/domain/member/repsoitory/MemberRepository.java
+++ src/main/java/com/example/blog/domain/member/repsoitory/MemberRepository.java
@@ -1,9 +1,14 @@
 package com.example.blog.domain.member.repsoitory;
 
+import com.example.blog.domain.member.entity.Member;
 import com.example.blog.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Post, Long> {
+
+    Optional<Member> findByusername(String username);
 }
