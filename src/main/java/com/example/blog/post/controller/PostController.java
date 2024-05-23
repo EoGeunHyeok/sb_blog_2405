7791ebfab33a2120745ffbc67579229823b76d53
EoGeunHyeok <eghsamo@naver.com>
2024-05-23 16:52:46 +0900

    게시글 리스트 데이터 출력

--- src/main/java/com/example/blog/post/controller/PostController.java
+++ src/main/java/com/example/blog/post/controller/PostController.java
@@ -1,14 +1,25 @@
 package com.example.blog.post.controller;
 
+import com.example.blog.post.entity.Post;
+import com.example.blog.post.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/post")
+@RequiredArgsConstructor
 public class PostController {
+    private final PostService postService;
     @GetMapping("/list")
-    public String list() {
+        public String list(Model model) {
+            List<Post> postList = postService.getList();
+
+            model.addAttribute("postList", postList);
         return "post/list";
     }
 }
